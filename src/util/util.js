c98e3bcc554e4c1bf828539747942c753bca8904
Ruben <rubendalssb@gmail.com>
2018-09-22 14:58:30 -0500

    Fix search highlighting

--- src/util/util.js
+++ src/util/util.js
@@ -60,9 +60,10 @@ export function FormatSearchScript(script, regex){
                 .replace(/\r\n/g, "</span><br/><span>")
                 .replace(/{<\/span><br\/><span>/g, "{</span><div class='script-tab'><span>")
                 .replace(/}<\/span><br\/><span>/g, "</span></div>}<br/><span>")
-                .replace(/(=)(-?[0-9A-F]+x?\.?[0-9A-F]*)(,|\))/g, "$1<span class='script-param-value'>$2</span>$3")
-                .replace(/([a-zA-Z_0-9/]+)(\()/g, "<span class='script-cmd'>$1</span>$2")
-                .replace(regex, "<span class='regex-match'>$1</span>");
+                .replace(regex, "<span class='regex-match'>$1</span>")
+                .replace(/(=)(-?[0-9A-F]+x?\.?[0-9A-F]*)(,|\)|<)/g, "$1<span class='script-param-value'>$2</span>$3")
+                .replace(/([a-zA-Z_0-9/]+)(\(|<)/g, "<span class='script-cmd'>$1</span>$2");
+                
 }
 
 export function FormatMscScript(script){
